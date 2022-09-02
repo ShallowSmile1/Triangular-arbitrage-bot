89a205066d5444f0c059f51a9772071d764f6ce7
Egor Gritsenko <Gritsenko_egor@mail.ru>
2022-09-02 22:16:34 +0300

    Initial commit

---
+++ myconfig.py
@@ -0,0 +1,3 @@
+# Update the API key and the API secret in this file and rename it to myconfig.py
+API_KEY = 'i3BsXW7KSgoeE0SNuWVnlKLBntO95s6SNCNA4fiz5YFZaczW4u3lfFEIuBQ0qnWs' 
+API_SECRET = 'HW0BPbSzzkYDndgt6Ddo63P7awDu1ZPOKd4neTKd6cSou73oyfuVRysCJg6ktdDL'
